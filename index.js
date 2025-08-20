9ea4eca9f09a49d9cd7a93331bf6eff3369592ec
Jared Cottenie <cotteniej@gmail.com>
2025-08-20 07:24:19 -0700

    Setup CLI foundation

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const chalk = require('chalk');
+
+program
+  .version('0.1.0')
+  .description('Content Commander - Manage your content ideas from the command line');
